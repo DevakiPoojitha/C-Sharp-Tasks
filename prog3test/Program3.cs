1c9734a69f2b882c0927ed709f20e76ded45a270
Devaki Poojitha <68280793+DevakiPoojitha@users.noreply.github.com>
2022-10-12 22:25:35 +0530

    Add files via upload

---
+++ prog3test/Program3.cs
@@ -0,0 +1,59 @@
+﻿using System.Reflection.Metadata;
+using System.Threading.Tasks;
+
+namespace prog3test
+{
+   public  interface IVehicle
+    {
+        void Drive();
+        bool Refuel(int amount);
+    }
+
+    
+    public class Car : IVehicle
+    {
+        public int fuel { get; set; }
+        int fu;
+        public Car(int fu)
+        {
+            this.fu = fu;    
+        }
+        public void Drive()
+        {
+            if(fu>0)
+                Console.WriteLine("Started Driving");
+            else
+            {
+                Console.WriteLine("refuel");
+            }
+            
+           
+        }
+        public bool Refuel(int amount)
+        {
+            if(fu>0)
+             return true;
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int f;
+            Console.WriteLine("Enter the amount of fuel");
+            f = Convert.ToInt32(Console.ReadLine());
+            Car mycar = new Car(f);
+            mycar.Drive();
+            Console.WriteLine(mycar.Refuel(f));
+            
+
+        }
+    }
+}
+
