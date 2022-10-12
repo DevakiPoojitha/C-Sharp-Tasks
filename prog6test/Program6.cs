69b553f583a07c9261922545e5fcaf1c5cc0ab14
Devaki Poojitha <68280793+DevakiPoojitha@users.noreply.github.com>
2022-10-12 22:28:24 +0530

    Add files via upload

---
+++ prog6test/Program6.cs
@@ -0,0 +1,13 @@
+﻿namespace prog6test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a C# program to copy the contents of one file to another file and encrypt the new file. Have any rule to differentiate the contents along with the encryption. 
+
+            File.Copy("C:\\Users\\poojitha.devaki\\file1.txt", "C:\\Users\\poojitha.devaki\\file2.txt");
+                Console.WriteLine(File.ReadAllText("file2.txt"));
+        }
+    }
+}
